342bf5b56d7e28d2b34722b30c38701222f68816
Prime Shadow <67257584+Prime-AI28@users.noreply.github.com>
2022-07-13 15:20:18 +0530

    Create ChainLink.sol

---
+++ ChainLink.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Rinkeby
+     * Aggregator: ETH/USD
+     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x031dB56e01f82f20803059331DC6bEe9b17F7fC9);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+        //1056.51366230
+        //0.36963800
+    }
+}
